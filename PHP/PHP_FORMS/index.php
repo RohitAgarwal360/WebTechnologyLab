4e5c10723e6debb83742abdf662923ba702ab657
StableWeaver <38763239+StableWeaver@users.noreply.github.com>
2019-02-05 12:49:00 +0530

    Add files via upload

---
+++ PHP/PHP_FORMS/index.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+	<div style="text-align:center">
+<form name="ss" action="Data.php" method="post">
+Name <input type=text name ="name" class="mdc-text-field__input"><br>
+ Age <input type=text name ="age" class="mdc-text-field__input">	<br>	
+ City <input type =text name ="city" class="mdc-text-field__input"><br>
+
+<input type=submit value="Submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+</form>
+
+</div>
+</body>
+
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
