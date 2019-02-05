dacacb87eda8d209cf4171fadecfb619f0369d54
StableWeaver <38763239+StableWeaver@users.noreply.github.com>
2019-02-05 12:42:00 +0530

    Add files via upload

---
+++ PHP_FORMS/config.php
@@ -0,0 +1,16 @@
+<html>
+	<body>
+		<?
+			$databaseHost="localhost";
+			$databaseuser="root";
+			$databaspassword="";
+			$databasename="test";
+
+			$mysql=mysqli_connect($databaseHost,$databaseuser,$databaspassword,$databasename);
+		?>
+	</body>
+
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
